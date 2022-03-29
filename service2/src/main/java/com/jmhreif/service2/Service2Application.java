a7018a08f636e0973bc9b48ae7975715d99d781e
Jennifer Reif <jennifer.reif@neo4j.com>
2022-03-29 13:54:44 -0500

    Fix code to expose client service

--- service2/src/main/java/com/jmhreif/service2/Service2Application.java
+++ service2/src/main/java/com/jmhreif/service2/Service2Application.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -27,10 +29,13 @@ public class Service2Application {
 }
 
 @Component
+@RestController
 @AllArgsConstructor
+@RequestMapping("/hello")
 class TextController {
 	private final WebClient client;
 
+	@GetMapping
 	Mono<String> getText() {
 		return client.get()
 				.uri("/text")
